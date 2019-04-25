2774bc4942ca36b23979cec356d5e1e9465d82d8
JeongJihye <jihye_0415@naver.com>
2019-04-25 18:08:33 +0900

    create project

---
+++ phone-book/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <PhoneForm></PhoneForm>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ phone-book/src/components/PhoneForm.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class PhoneForm extends Component {
+    state = {
+        name: '',
+        phone: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <form>
+                <input placeholder="이름" value={this.state.name} onChange={this.handleChange} name="name"></input>
+                <input placeholder="전화번호" value={this.state.phone} onChange={this.handleChange} name="phone"></input>
+                <div>{this.state.name} {this.state.phone}</div>
+            </form>
+        );
+    }
+}
+
+export default PhoneForm;
