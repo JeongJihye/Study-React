a44ff1cda84d24a511f1ee2ff977833dbae1492a
JeongJihye <jihye_0415@naver.com>
2019-04-26 10:24:38 +0900

    key

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm.js';
+import PhoneInfoList from './components/PhoneInfoList.js';
 
 class App extends Component {
-  handleCreate = (data) => {
-    console.log(data);
+  id = 2
+  state = {
+    information : [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '000-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '111-1111-1111'
+      }
+    ]
   }
-  
+
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
+  }
+
   render() {
+    const { information } = this.state;
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate}></PhoneForm>
+        <PhoneInfoList data={this.state.information}></PhoneInfoList>
       </div>
     );
   }
