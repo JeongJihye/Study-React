fcf4533103a7b9e6850e16c71b711f2d37b475d6
JeongJihye <jihye_0415@naver.com>
2019-05-08 16:14:10 +0900

    state

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './Components/Movie.js';
+import Movie from './Components/Movie.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    movies : [
+      {
+        title : "Iron Man",
+        poster : "https://movie-phinf.pstatic.net/20111222_37/1324501632182vbSoY_JPEG/movie_image.jpg"
+      },
+      {
+        title : "Thor",
+        poster : "https://movie-phinf.pstatic.net/20111222_37/1324501632182vbSoY_JPEG/movie_image.jpg"
+      },
+      {
+        title : "Hulk",
+        poster : "https://movie-phinf.pstatic.net/20111222_37/1324501632182vbSoY_JPEG/movie_image.jpg"
+      },
+      {
+        title : "Captin America",
+        poster : "https://movie-phinf.pstatic.net/20111222_37/1324501632182vbSoY_JPEG/movie_image.jpg"
+      }
+    ]
+  }
+
+  componentWillMount() {
+    console.log("componentWillMount");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          ...this.state.movies, //기존 목록 유지
+          {
+            title : "Black Panther",
+            poster : "https://movie-phinf.pstatic.net/20111222_37/1324501632182vbSoY_JPEG/movie_image.jpg"
+          }
+        ]
+      })
+    }, 5000)
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div className="App">
+        {this.state.greeting}
+        {this.state.movies.map((movie, index) => {return <Movie title={movie.title} poster={movie.poster} key={index}></Movie>})}
+      </div>
+    );
+    }
 }
 
 export default App;
